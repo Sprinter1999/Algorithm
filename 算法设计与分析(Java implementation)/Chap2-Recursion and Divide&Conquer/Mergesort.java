a61a922a52f8d814a47ea100c504ff22d28ae869
Xuefen <37746681+Sprinter1999@users.noreply.github.com>
2019-07-19 20:39:51 +0800

    Update Mergesort.java

--- 算法设计与分析(Java implementation)/Chap2-Recursion and Divide&Conquer/Mergesort.java
+++ 算法设计与分析(Java implementation)/Chap2-Recursion and Divide&Conquer/Mergesort.java
@@ -1,71 +1,40 @@
-public class Solution 
-{
-    /**
-     * @param A: an integer array
-     * @return: nothing
-     */
-    public void sortIntegers(int[] A) 
-    {
-        if(A == null || A.length <= 1)//对输入数据进行检查
-        {
-            return;
-        }
-        int[] tempA = new int[A.length];//一个临时数组，在这里申请，可以反复利用，提高效率
-        mergeSort(A, tempA, 0, A.length - 1);
-    }
-    
-    private void mergeSort(int[] A, int[] tempA, int left, int right)
-    {
-        if(left == right)//递归退出的条件（只有一个元素时，不需要再拆分
-        {
-            return;
-        }
-        int middle = (left + right)/2;
-        mergeSort(A, tempA, left, middle);//左边拆分
-        mergeSort(A, tempA, middle + 1, right);//右边拆分
-        merge(A, tempA, left, middle, right);//合并两个有序数组
-    }
-    
-    /**
-    * 合并两个有序数组
-    * 这两个有序数组在A中，
-    * left和right分别为两个子数组的左右边界，middle为这两个数组的分界点
-    * tempA为临时数组，排序后暂存在此数组中，排序后再赋给A
-    */
-    private void merge(int[] A, int[] tempA, int left, int middle, int right)
-    {
-        int i = left;//i指向左数组的第一位
-        int j = middle + 1;//j指向右数组的第一位
-        int k = 0;//用来方便将排序后的数存放到tempA
-        while(i <= middle && j <= right)
-        {
-            if(A[i] < A[j])
-            {
-                tempA[k] = A[i];
-                i++;
-            }
+import ...
+public class MergeSort {
+    //两路归并算法，两个排好序的子序列合并为一个子序列
+    public void merge(int []a,int left,int mid,int right){
+        int []tmp=new int[a.length];//辅助数组
+        int p1=left,p2=mid+1,k=left;//p1、p2是检测指针，k是存放指针
+
+        while(p1<=mid && p2<=right){
+            if(a[p1]<=a[p2])
+                tmp[k++]=a[p1++];
             else
-            {
-                tempA[k] = A[j];    
-                j++;
-            }
-            k++;
+                tmp[k++]=a[p2++];
         }
-        
-        while(i <= middle)//如果左数组有剩余，全部加入tempA
-        {
-            tempA[k++] = A[i++];
+
+        while(p1<=mid) tmp[k++]=a[p1++];//如果第一个序列未检测完，直接将后面所有元素加到合并的序列中
+        while(p2<=right) tmp[k++]=a[p2++];//同上
+
+        //复制回原素组
+        for (int i = left; i <=right; i++) 
+            a[i]=tmp[i];
+    }
+
+    public void mergeSort(int [] a,int start,int end){
+        if(start<end){//当子序列中只有一个元素时结束递归
+            int mid=(start+end)/2;//划分子序列
+            mergeSort(a, start, mid);//对左侧子序列进行递归排序
+            mergeSort(a, mid+1, end);//对右侧子序列进行递归排序
+            merge(a, start, mid, end);//合并
         }
-        
-        while(j <= right)//如果右数组有剩余，全部加入tempA
-        {
-            tempA[k++] = A[j++];
-        }
-        
-        //将排序后的数替换A中未排序的数，注意：不是整个替换
-        for(int index = 0; index < k; index++)
-        {
-            A[left + index] = tempA[index];//注意A的索引，[left + index];
-        }
+    }
+
+    @Test
+    public void test(){
+        int[] a = { 49, 38, 65, 97, 76, 13, 27, 50 };
+        mergeSort(a, 0, a.length-1);
+        System.out.println("排好序的数组：");
+        for (int e : a)
+            System.out.print(e+" ");
     }
 }
